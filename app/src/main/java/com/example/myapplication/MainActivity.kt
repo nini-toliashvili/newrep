7bdd128ba163f037d23dfd935a665276b06026d7
nini-toliashvili <ntoliashvili4@gmail.com>
2024-12-22 01:43:14 +0400

    tavidan mewera fragmentebis gareshe, shemdeg sakmaod gvian gadavwere davaleba fragmentebit tumca araa gamartuli da arc sakitxi mesmis kargad amitom davubrune tavdapirvel formas.

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,5 +1,12 @@
 package com.example.myapplication
+import android.graphics.Color
 import android.os.Bundle
+import android.text.SpannableString
+import android.text.Spanned
+import android.text.method.LinkMovementMethod
+import android.text.style.ClickableSpan
+import android.text.style.ForegroundColorSpan
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import com.example.myapplication.databinding.ActivityMainBinding
 
@@ -11,14 +18,50 @@ private lateinit var binding :ActivityMainBinding
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        if (savedInstanceState == null) {
-            supportFragmentManager.
-            beginTransaction().
-                    add(R.id.main, Fragment1()).commit()
-        }
+        setUp()
     }
 
 
+
+
+
+
+    private fun setUp() {
+        val text = "Don't have an account? Sign up"
+        val spannableString = SpannableString(text)
+        spannableString.setSpan(ForegroundColorSpan(Color.BLACK), 23, 30, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE)
+        spannableString.setSpan(object : ClickableSpan(){
+            override fun onClick(widget: View) {
+                binding.signin.visibility = View.GONE
+                binding.signUp.visibility = View.VISIBLE
+
+            }
+    }, 23, 30, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE)
+        binding.text.text = spannableString
+        binding.text.movementMethod = LinkMovementMethod.getInstance()
+
+
+
+
+
+
+
+
+
+
+        val textSignin = "Already have an account? Sign in"
+        val spannable = SpannableString(textSignin)
+        spannable.setSpan(ForegroundColorSpan(Color.BLACK), 25, 32, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE)
+        spannable.setSpan(object : ClickableSpan(){
+            override fun onClick(widget: View) {
+                binding.signUp.visibility = View.GONE
+                binding.signin.visibility = View.VISIBLE
+            }
+        }, 23, 30, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE)
+        binding.signintext.text = spannable
+        binding.signintext.movementMethod = LinkMovementMethod.getInstance()
+
+    }
 
 
 }
